e495c9b81b7326b7a28f796b9f97f26b6d1cda95
Saloni Sharma <68701930+saloni1966@users.noreply.github.com>
2021-09-05 19:04:38 +0530

    Update Atm.py

--- Atm.py
+++ Atm.py
@@ -1,13 +1,34 @@
-class Atm(object):
-    def __init__(self,cardNumber=None, pinNumber=None):
-        self.cardNumber = {}
-        self.pinNumber = {}
-    
-    def CashWithdrawl(self):
-        print("How much cash would you like to withdraw? Please type out below.")
+class Atm:
+    def __init__(self,cardnumber,pin):
+        self.cardnumber = cardnumber
+        self.pin = pin
 
-    def BalanceEnquiry(self):
-        print("Enter you Card Number as well as the Pin Number here. Note: The process is entirely secured :)")
+    def check_balance(self):
+        print("Your balance is 50000")
 
-    def PayBills(self):
-        int(input("Please enter the number on which you want the pay the bill to - "))
+    def withdrawl(self,amount):
+        new_amount = 50000 - amount
+        print("you have withdrawn amount "+str(amount) +". Your remaining balance is "+ str(new_amount))
+
+
+def main():
+    Card_number = input("insert your card number:- ")
+    pin_number  = input("enter your pin number:- ")
+
+    new_user =  Atm(Card_number ,pin_number)
+
+    print("Choose your activity ")
+    print("1.Balance Enquriy   2.withdrawl")
+    activity = int(input("enter activity number :- "))
+
+    if (activity == 1):
+        new_user.check_balance()
+    elif (activity == 2):
+        amount = int(input("enter the amount:- "))
+        new_user.withdrawl(amount)
+    else:
+        print("enter a valid number")
+
+
+if __name__ == "__main__":
+    main()
